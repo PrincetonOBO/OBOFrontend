1ee883b4d0f372247bb411b395334f5b0deb0d67
Catherine Wu <catherine.cw.wu@gmail.com>
2015-03-30 20:04:47 -0400

    initial take 2

--- OBO/OBOSellThingViewController.m
+++ OBO/OBOSellThingViewController.m
@@ -9,13 +9,23 @@
 #import "OBOSellThingViewController.h"
 @interface OBOSellThingViewController()
 @property (weak, nonatomic) IBOutlet UITextField *itemDescriptionTextField;
+@property (weak, nonatomic) IBOutlet UITextField *itemNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *itemPriceTextField;
+@property (weak, nonatomic) IBOutlet UITextField *itemSizeTextField;
+@property (weak, nonatomic) IBOutlet UIPickerView *itemPricePickerView;
 
 @end
 
 @implementation OBOSellThingViewController
 - (IBAction)submit:(id)sender {
+    NSString *name = self.itemNameTextField.text;
+    NSString *price = self.itemPriceTextField.text;
+    NSString *size = self.itemSizeTextField.text;
     NSString *text = self.itemDescriptionTextField.text;
     self.itemDescriptionTextField.text = @"";
+    self.itemNameTextField.text = @"";
+    self.itemPriceTextField.text = @"";
+    self.itemSizeTextField.text = @"";
 }
 
 @end
